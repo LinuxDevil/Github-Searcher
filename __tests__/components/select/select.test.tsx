9f10d487b4cacd7cb42fd0575aa10bc2f795ab28
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-11 14:18:33 +0300

    feat: select change event test

--- __tests__/components/select/select.test.tsx
+++ __tests__/components/select/select.test.tsx
@@ -1,20 +1,33 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Select from "@/components/select/select";
 
 describe("Select", () => {
 
-  it("should render the select", () => {
-    const options = [
-      { label: 'Users', value: 'user' },
-      { label: 'Repositories', value: 'repo' }
-    ];
-    const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {};
+  const options = [
+    { label: 'Users', value: 'user' },
+    { label: 'Repositories', value: 'repo' }
+  ];
+
+  const onChange = jest.fn();
+
+  beforeEach(() => {
     render(<Select defaultValue={'user'} options={options} onChange={onChange} />);
+  });
+
+  it("should render the select", () => {
     expect(screen.getByRole("combobox")).toBeInTheDocument();
     expect(screen.getByRole("option", { name: 'Users' })).toBeInTheDocument();
     expect(screen.getByRole("option", { name: 'Repositories' })).toBeInTheDocument();
     expect(screen.getByRole("option", { name: 'Users' })).toHaveValue('user');
     expect(screen.getByRole("option", { name: 'Repositories' })).toHaveValue('repo');
   });
+
+  it("should call onChange when select value changes", () => {
+    const select = screen.getByRole("combobox");
+    fireEvent.change(select, { target: { value: 'repo' } });
+    expect(select).toHaveValue('repo');
+    expect(onChange).toHaveBeenCalled();
+  });
+
 });
