cb620ab33c5048ca9a90a1ea3d6294e4cdacb367
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-11 23:07:26 +0300

    feat: card component

---
+++ src/components/shared/card/card.tsx
@@ -0,0 +1,10 @@
+import "./card.scss";
+import { ReactNode } from "react";
+
+export default function Card({ children }: { children: ReactNode }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
