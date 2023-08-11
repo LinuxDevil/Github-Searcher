661d3b179afe277f6e1ffc83033cb0720998addd
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 00:30:56 +0300

    feat: create card test

---
+++ __tests__/components/shared/card/card.test.tsx
@@ -0,0 +1,17 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Card from "@/components/shared/card/card";
+
+describe("Card", () => {
+
+  beforeEach(() => {
+    render(<Card> <p>Testing the card</p> </Card>);
+  });
+
+  it("should render the card", () => {
+    const card = screen.getByText('Testing the card').parentElement;
+    expect(card).toBeInTheDocument();
+    expect(card).toHaveClass('card');
+  });
+
+});
