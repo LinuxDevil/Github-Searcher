3f6038b401207bca4e868ffb4ac819c3ef5e41c3
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 19:10:39 +0300

    refactor(core): split github list into search loading and no-result components

--- src/components/feature/github-list/github-list.tsx
+++ src/components/feature/github-list/github-list.tsx
@@ -5,8 +5,10 @@ import GithubUser from "@/components/feature/github-user/github-user";
 import { buildQuery } from "@/app/search/searcher.service";
 import useSWRMutation from "swr/mutation";
 import { apiFetcher } from "@/network/api-fetcher";
-import Image from "next/image";
 import GithubRepository from "@/components/feature/github-repository/github-repository";
+import { Loading } from "@/components/feature/github-list/loading";
+import { ResultHeader } from "@/components/feature/github-list/result-header";
+import { NoResults } from "@/components/feature/github-list/no-results";
 
 export default function GithubList() {
 
@@ -22,13 +24,11 @@ export default function GithubList() {
     displayedResultType,
     isLoading,
     setIsLoading,
-    addNewResults,
+    addNewResults
   } = useContext(SearcherContextData);
 
-  const noResults = query ? 'No results! 😿' : 'Try to search for something! 👀';
-
   const DisplayTemplate = useMemo(() => {
-    return displayedResultType === 'users' ? GithubUser : GithubRepository
+    return displayedResultType === "users" ? GithubUser : GithubRepository;
   }, [displayedResultType]);
 
   const gitHubEndPoint = buildQuery(query, page, pageSize, searchType);
@@ -54,22 +54,10 @@ export default function GithubList() {
   };
 
   return (
-    <div data-testid='github-list' className="github-list" ref={listRef} onScroll={onScroll}>
-      {results.length === 0 && !isLoading &&
-        <div data-testid='github-list__empty' className="github-list__empty">
-          <Image src={"/images/no-results__logo.png"} alt={"No results logo"} width={128} height={128} />
-          <div className="github-list__empty-text">{noResults}</div>
-        </div>}
-      {results.length > 0 && !isLoading &&
-        <div className="github-list__results">
-          Results: {total} {displayedResultType}
-        </div>}
-      {isLoading &&
-        <div className="github-list__loading">
-          <Image className='github-list__loading__logo' src={"/images/no-results__logo.png"} alt={"Loading logo"} width={128} height={128} />
-          <div className="github-list__loading__text">Searching...</div>
-        </div>
-      }
+    <div data-testid="github-list" className="github-list" ref={listRef} onScroll={onScroll}>
+      {total === 0 && !isLoading && <NoResults />}
+      {total > 0 && !isLoading && <ResultHeader total={total} displayedResultType={displayedResultType} />}
+      {isLoading && <Loading />}
       {results.map((result: GithubAPIResultsType, index) => (
         <DisplayTemplate key={`${result.id}${result.node_id}${index}`} {...result} />
       ))}
