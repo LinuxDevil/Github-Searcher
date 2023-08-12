0dc3f71441cfcaab7f48167e39a407fe2b927f6e
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 13:40:04 +0300

    fix: remove unused variables from github-api model

--- src/models/github-api.ts
+++ src/models/github-api.ts
@@ -1,10 +1,7 @@
-import { GithubAPIResultsType } from "@/state/searcher.context";
-
 export type GithubAPISearchType = 'repositories' | 'users';
 
 export interface IGithubAPIResponse {
   total_count: number;
-  incomplete_results: boolean;
   items: IGithubRepository[] | IGithubUser[];
 }
 
@@ -19,47 +16,12 @@ export interface IGithubUser {
 export interface IGithubRepository {
   id: number;
   node_id: string;
-  name: string;
   full_name: string;
-  [`private`]: boolean;
-  owner: IGithubUser;
-  html_url: string;
   description: string;
   fork: boolean;
-  url: string;
   forks_url: string;
-  keys_url: string;
-  collaborators_url: string;
-  teams_url: string;
-  hooks_url: string;
-  issue_events_url: string;
-  events_url: string;
-  assignees_url: string;
-  branches_url: string;
-  tags_url: string;
-  blobs_url: string;
-  git_tags_url: string;
-  git_refs_url: string;
-  trees_url: string;
-  statuses_url: string;
   languages_url: string;
-  stargazers_url: string;
-  contributors_url: string;
-  subscribers_url: string;
-  subscription_url: string;
-  commits_url: string;
-  git_commits_url: string;
-  comments_url: string;
-  issue_comment_url: string;
-  contents_url: string;
-  compare_url: string;
-  merges_url: string;
-  archive_url: string;
-  downloads_url: string;
-  issues_url: string;
-  pulls_url: string;
-  milestones_url: string;
-  notifications_url: string;
+  owner: IGithubUser;
 }
 
 export interface IGithubRepositoryLanguages {
