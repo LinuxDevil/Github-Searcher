ca23647a7d7b10e5b037d37d3ed6f7ff2648e570
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 22:37:58 +0300

    fix: replaced the refactored image name to logo and removed extra logo from github list

--- src/components/feature/github-list/loading.tsx
+++ src/components/feature/github-list/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Loading = () => (
   <div className="github-list__loading">
-    <Image className="github-list__loading__logo" src={"/images/no-results__logo.png"} alt={"Loading logo"}
+    <Image className="github-list__loading__logo" src={"/images/github.png"} alt={"Loading logo"}
            width={128} height={128} />
     <div className="github-list__loading__text">Searching...</div>
   </div>
