0b0aed3459cbc415a8b28baea7abe723a82ee456
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 14:24:44 +0300

    feat: create github-user test and added data-testid for the github-user container

---
+++ __tests__/components/feature/github-user/github-user.test.tsx
@@ -0,0 +1,30 @@
+import "@testing-library/jest-dom";
+import { queryAllByAltText, queryByAttribute, render, screen, waitFor } from "@testing-library/react";
+import { fetchGithubExtraData } from "@/app/search/searcher.service";
+import GithubLanguages from "@/components/feature/github-languages/github-languages";
+import { IGithubUser } from "@/models/github-api";
+import GithubUser from "@/components/feature/github-user/github-user";
+
+describe("<GithubUser />", () => {
+
+  it ('should render', () => {
+    const mockUser: IGithubUser = {
+      login: 'testing-login',
+      html_url: 'testing-html-url',
+      avatar_url: 'http://testing-avatar-url',
+      id: 1,
+      node_id: 'testing-node-id',
+    }
+
+    render(<GithubUser {...mockUser} />);
+
+    expect(screen.getByTestId('github-user')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByRole('img')).toHaveAttribute('alt', `Github user ${mockUser.login} avatar`);
+    expect(screen.getByText(mockUser.login)).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+    expect(screen.getByRole('link')).toHaveAttribute('href', mockUser.html_url);
+    expect(screen.getByRole('link')).toHaveTextContent('Profile');
+  });
+
+});
--- src/components/feature/github-user/github-user.tsx
+++ src/components/feature/github-user/github-user.tsx
@@ -8,7 +8,7 @@ export default function GithubUser({ login, html_url, avatar_url }: IGithubUser)
 
   return (
     <Card>
-      <div className="github-user">
+      <div data-testid='github-user' className="github-user">
         <Image className='github-user__avatar' src={avatar_url} alt={`Github user ${login} avatar`} width={64} height={64} />
         <div className="github-user__info">
           <div className="github-user__login">{login}</div>
