299f17da5c4c66c0d3bedbb149bd9ac9f9eda9fc
Ali Mohammad <freelancealimohammad@gmail.com>
2023-08-12 15:14:09 +0300

    fix: removed throttle from scroll bar

--- src/components/feature/github-list/github-list.tsx
+++ src/components/feature/github-list/github-list.tsx
@@ -1,17 +1,16 @@
 import "./github-list.scss";
 import { useContext, useMemo, useRef, useState } from "react";
-import { SearcherContextData } from "@/state/searcher.context";
+import { GithubAPIResultsType, SearcherContextData } from "@/state/searcher.context";
 import GithubUser from "@/components/feature/github-user/github-user";
 import { buildQuery } from "@/app/search/searcher.service";
 import useSWRMutation from "swr/mutation";
 import { apiFetcher } from "@/network/api-fetcher";
 import Image from "next/image";
 import GithubRepository from "@/components/feature/github-repository/github-repository";
-import { throttle } from "lodash";
+
 export default function GithubList() {
 
   const listRef = useRef<HTMLDivElement>(null);
-  const [isLFetching, setIsFetching] = useState(false);
 
   const {
     query,
@@ -21,28 +20,29 @@ export default function GithubList() {
     total,
     results,
     isLoading,
+    setIsLoading,
     displayedResultType,
-    addNewResults
+    addNewResults,
   } = useContext(SearcherContextData);
 
   const noResults = query ? 'No results! 😿' : 'Try to search for something! 👀';
 
   const DisplayTemplate = useMemo(() => {
-    return searchType === 'users' ? GithubUser : GithubRepository
-  }, [searchType]);
+    return displayedResultType === 'users' ? GithubUser : GithubRepository
+  }, [displayedResultType]);
 
   const gitHubEndPoint = buildQuery(query, page, pageSize, searchType);
   const { trigger } = useSWRMutation(gitHubEndPoint, apiFetcher);
 
   const fetchResults = async () => {
-    if (isLFetching) return;
-    setIsFetching(true);
+    if (isLoading) return;
+    setIsLoading(true);
     const pageCount = Math.ceil(results.length / pageSize);
     if (page > pageCount) return;
 
     const { items, total_count } = await trigger();
     addNewResults(total_count, items);
-    setIsFetching(false);
+    setIsLoading(false);
   };
 
   const onScroll = () => {
@@ -53,10 +53,8 @@ export default function GithubList() {
     }
   };
 
-  const throttledOnScroll = throttle(onScroll, 200);
-
   return (
-    <div className="github-list" ref={listRef} onScroll={throttledOnScroll}>
+    <div className="github-list" ref={listRef} onScroll={onScroll}>
       {results.length === 0 && !isLoading &&
         <div className="github-list__empty">
           <Image src={"/images/no-results__logo.png"} alt={"No results logo"} width={128} height={128} />
@@ -72,8 +70,8 @@ export default function GithubList() {
           <div className="github-list__loading__text">Searching...</div>
         </div>
       }
-      {results.map((result, index) => (
-        <DisplayTemplate key={`${result.id}${result.node_id}`} {...result} />
+      {results.map((result: GithubAPIResultsType, index) => (
+        <DisplayTemplate key={`${result.id}${result.node_id}${index}`} {...result} />
       ))}
     </div>
   );
